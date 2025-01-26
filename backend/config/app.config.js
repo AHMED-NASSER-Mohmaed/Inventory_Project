dcbd4c37a3be163648f67ef1a2c8b0af16d22151
fairouzmagdy <fairouzmagdy219@gmail.com>
2025-01-26 23:51:17 +0200

    fix: fix db uri

--- backend/config/app.config.js
+++ backend/config/app.config.js
@@ -1,4 +1,8 @@
 module.exports.APP_CONFIG = {
-  DATABASE: process.env.DATABASE,
-  HTTP_PORT: 3000,
+  MONGO_DEV_URI: process.env.MONGO_DEV_URI,
+  MONGO_DATABASE_NAME: process.env.MONGO_DATABASE_NAME,
+  HTTP_PORT: process.env.HTTP_PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 };
--- backend/main.js
+++ backend/main.js
@@ -3,18 +3,33 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
 const { APP_CONFIG } = require("./config/app.config");
+const { DATABASE } = require("./database/mongo/index");
 const app = require("./app");
 const port = APP_CONFIG.HTTP_PORT || 5000;
 
-mongoose
-  .connect(APP_CONFIG.DATABASE)
-  .then(async () => {
-    console.log("DB connection successful");
-  })
-  .catch((err) => {
-    console.error(`Error connecting to the database: ${err}`);
-  });
-
-app.listen(port, () => {
-  console.log(`Starting server on port ${port}`);
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection! Shutting down...");
+  console.error(err);
+  process.exit(1);
 });
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception! Shutting down...");
+  console.error(err);
+  process.exit(1);
+});
+
+(async function () {
+  await DATABASE.connectToMongo({
+    dbOptions: {
+      url: APP_CONFIG.MONGO_DEV_URI,
+      databaseName: APP_CONFIG.MONGO_DATABASE_NAME,
+    },
+    callback: () => {
+      console.log("App database has connected successfully");
+      app.listen(APP_CONFIG.HTTP_PORT, "0.0.0.0", () => {
+        console.log(`App is up and running on port ${APP_CONFIG.HTTP_PORT}`);
+      });
+    },
+  });
+})();
