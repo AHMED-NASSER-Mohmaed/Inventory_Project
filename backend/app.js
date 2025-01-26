d36aa9930c99b7c0496b79d56c2273233183ed07
fairouzmagdy <fairouzmagdy219@gmail.com>
2025-01-26 20:35:52 +0200

    fix: fix middleware path in app

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const fs = require("fs");
-const globalErrorHandler = require("./controllers/error.controller");
+const globalErrorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 
