dd17f430805e2cac5ed022452ff6c9dc03258933
Ahmed Nasser <ahmed.nasser.elgendy0@gmail.com>
2025-02-09 18:00:30 +0200

    dashboard+home are merged

--- frontend/src/app/Components2/HomePage/landing-page/landing-page.component.ts
+++ frontend/src/app/Components2/HomePage/landing-page/landing-page.component.ts
@@ -3,11 +3,13 @@ import { SliderComponent } from '../slider/slider.component';
 import { CategoryComponent } from '../category/category.component';
 import { FeaturedProductsComponent } from '../featured-products/featured-products.component';
 import { SaleComponent } from '../sale/sale.component';
+import { HeaderComponent } from '../../../core/header/header.component';
+import { FooterComponent } from '../../../core/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   imports: [SliderComponent, CategoryComponent, FeaturedProductsComponent, SaleComponent,  
-  ],
+  HeaderComponent,FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
