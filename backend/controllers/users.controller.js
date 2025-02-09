d4c5ce5694b80af2078c0a6d4d7719045b91fe2d
Ahmed Nasser <ahmed.nasser.elgendy0@gmail.com>
2025-02-09 16:58:18 +0200

    update image profile is updated due to image id became an array

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -372,7 +372,7 @@ const customerOp = {
             //delete image from imagekit  if user it's not the default image
             if (!(oldFileId['photo']['fileId'] === APP_CONFIG.UDIAMGE_ID_VALUE)) {
                 console.log("the one that is exist is not equal to the default one");
-                await deleteFiles(oldFileId['photo']['fileId']);
+                await deleteFiles([oldFileId['photo']['fileId']]);
             }
 
             imageInfo = await upload(req.files, APP_CONFIG.PROFILE_IMAGE_FOLDER);
@@ -384,7 +384,7 @@ const customerOp = {
         } finally {
 
             if (!imageInfo) {
-                await userService.updateUserImage(id, APP_CONFIG.DU_IMAGE_DEFALUT_OBG);
+                await userService.updateUserImage(req.params.id, APP_CONFIG.DU_IMAGE_DEFALUT_OBG);
                 throw new AppError(error.message, APP_CONFIG.HTTP_INTERNAL_SERVER_ERROR);
             }
 
