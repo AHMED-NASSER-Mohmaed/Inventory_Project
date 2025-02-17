165f96eb1d276b1e9319ec50737fb578d0446d97
Ahmed Nasser <ahmed.nasser.elgendy0@gmail.com>
2025-02-17 23:51:43 +0200

    some whitespaces

--- backend/models/seller.model.js
+++ backend/models/seller.model.js
@@ -7,15 +7,12 @@ const SellerSchema = new mongoose.Schema(
     SSN: { type: String, required: true, unique:true},
     companyName: { type: String, required: true ,  unique:true},
     companyRegistrationNumber: { type: String, required: true, unique:true },
-<<<<<<< HEAD
-=======
     
 
     // 0 --> represent pending
     // 1 --> approved
     //-1 --> rejected
 
->>>>>>> main
     status:{
       type: Number,
       default: 0,
