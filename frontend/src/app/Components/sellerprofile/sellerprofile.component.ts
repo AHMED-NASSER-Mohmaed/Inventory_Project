2b6ad9b8c498fac08858a6f4577b59e845b0ef8e
AbdulrhmannAhmedd <abdulrhmann.ahmedd@gmail.com>
2025-03-05 01:33:44 +0200

    seller can change its profiel picture now

--- frontend/src/app/Components/sellerprofile/sellerprofile.component.ts
+++ frontend/src/app/Components/sellerprofile/sellerprofile.component.ts
@@ -8,6 +8,8 @@ import { Router, RouterLink } from '@angular/router';
 import { AccountService } from '../../_services/account.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmLogoutDialogComponent } from '../../confirm-logout-dialog/confirm-logout-dialog.component';
+import { ConfirmDialogImgchangeComponent } from '../../confirm-dialog-imgchange/confirm-dialog-imgchange.component';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-sellerprofile',
   imports: [FormsModule , CommonModule , RouterLink],
@@ -15,7 +17,7 @@ import { ConfirmLogoutDialogComponent } from '../../confirm-logout-dialog/confir
   styleUrl: './sellerprofile.component.css'
 })
 export class SellerprofileComponent implements OnInit {
-  constructor(public customerProfileService: CustomersProfileService ,public accountService: AccountService, public dialog: MatDialog, public router: Router){}
+  constructor(public customerProfileService: CustomersProfileService ,public accountService: AccountService, public dialog: MatDialog, public router: Router, private toastr: ToastrService){}
 
   isEditing = false;
   sub = {} as Subscription;
@@ -55,6 +57,64 @@ export class SellerprofileComponent implements OnInit {
        }
      });
    }
+
+   triggerImageUpload() {
+         document.getElementById('imageUpload')?.click();
+       }
+     
+       // onImageChange(event: any) {
+       //   const file = event.target.files[0];
+       //   if (file) {
+       //     const reader = new FileReader();
+       //     reader.onload = (e: any) => {
+       //       document.querySelector('.firstImage')?.setAttribute('src', e.target.result);
+       //     };
+       //     reader.readAsDataURL(file);
+       //   }
+       // }
+   
+   
+   
+   
+   
+       async onImageChange(event: any): Promise<void> {
+         const fileInput = event.target;
+         const file = fileInput.files[0];
+           const backupUrl = this.userP;
+           const reader = new FileReader();
+           reader.onload = (e: any) => {
+             const tempUrl = e.target.result;
+             const dialogRef = this.dialog.open(ConfirmDialogImgchangeComponent);
+             const sub = dialogRef.afterClosed().subscribe(async (result) => {
+               if (result) {
+                 try {
+                   const response: any = await this.customerProfileService
+                     .changeImage(file)
+                     .toPromise();
+                   if (response.message === 'success') {
+                     this.userP = tempUrl;
+                   } else {
+                     this.userP = backupUrl;
+                   }
+                 } catch (error) {
+                   this.toastr.clear();
+                   this.toastr.error((error as any).error.message, 'Failed', {
+                     timeOut: 1500,
+                     positionClass: 'toast-bottom-right',
+                     progressBar: true,
+                     closeButton: true
+                   });
+                   console.error('Error updating image', error);
+                   this.userP = backupUrl;
+                 }
+               } else {
+                 this.userP = backupUrl;
+               }
+               fileInput.value = '';
+             });
+           };
+           reader.readAsDataURL(file);
+       }
 
   
 
